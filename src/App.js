c83ae405eee48425dce7b85b0dac5f8c2cde4fec
sagostinelli <batonenbois@gmail.com>
2020-08-09 07:46:31 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import EthLogo from './eth-animation.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SVG animation done with SVGator!!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <object type="image/svg+xml" data={EthLogo} className="eth-logo">svg-animation</object>
       </header>
     </div>
   );
